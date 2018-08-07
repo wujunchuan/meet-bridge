a416e7a690253b579be18c7f99c8d5e8288dfba5
JohnTrump <wujunchuan2008@gmail.com>
2018-08-07 15:08:23 +0800

    feat: 增加协议

--- src/meet-bridge.ts
+++ src/meet-bridge.ts
@@ -2,8 +2,8 @@
  * MEET桥接
  * @Author: JohnTrump
  * @Date: 2018-08-06 16:26:02
- * @Last Modified by:   JohnTrump
- * @Last Modified time: 2018-08-06 16:26:02
+ * @Last Modified by: JohnTrump
+ * @Last Modified time: 2018-08-07 15:07:57
  */
 
 export default class Bridge {
@@ -15,7 +15,7 @@ export default class Bridge {
 
   // 将对象转换成URL参数并且完成转码
   // 编码过程： JSON化 -> url编码 -> base64编码
-  private _coverObjectToParams(obj: object): string {
+  public static coverObjectToParams(obj: object): string {
     try {
       let json = JSON.stringify(obj)
       return btoa(encodeURIComponent(json))
@@ -27,7 +27,7 @@ export default class Bridge {
 
   // 将URL参数解码并且转换成对象
   // 解码过程： base64解码 -> url解码 -> JSON化
-  private _revertParamsToObject(url: string): object {
+  public static revertParamsToObject(url: string): object {
     try {
       // decode base64
       const decodeURL = atob(url)
@@ -39,24 +39,105 @@ export default class Bridge {
     return {}
   }
 
-  // 生成URI地址
-  public generateURI({ routeName = 'eos/transfer', params = {} }): string {
+  // 解析URL
+  public static parseURL(url: string): object {
+    let a = document.createElement('a')
+    a.href = url
+    return {
+      source: url,
+      protocol: a.protocol.replace(':', ''),
+      host: a.hostname,
+      port: a.port,
+      query: a.search,
+      params: (function() {
+        let ret = {},
+          seg = a.search.replace(/^\?/, '').split('&'),
+          len = seg.length,
+          i = 0,
+          s
+        for (; i < len; i++) {
+          if (!seg[i]) {
+            continue
+          }
+          s = seg[i].split('=')
+          ret[s[0]] = s[1]
+        }
+        return ret
+      })(),
+      file: (a.pathname.match(/\/([^\/?#]+)$/i) || [, ''])[1],
+      hash: a.hash.replace('#', ''),
+      path: a.pathname.replace(/^([^\/])/, '/$1'),
+      relative: (a.href.match(/tps?:\/\/[^\/]+(.+)/) || [, ''])[1],
+      segments: a.pathname.replace(/^\//, '').split('/')
+    }
+  }
+
+  // 生成协议URI地址
+  public generateURI({ routeName = '', params = {}, callbackId = '' }): string {
     return this.schema
       .concat(routeName)
       .concat('?params=')
-      .concat(this._coverObjectToParams(params))
-      .concat('&callbackId=' + '101')
+      .concat(Bridge.coverObjectToParams(params))
+      .concat('&callbackId=' + callbackId)
   }
 
-  // getInfo(callback: any) {
-  //   this.eos.getInfo({}).then((res: any) => {
-  //     callback && callback(res)
-  //   })
-  // }
+  // 请求获取授权
+  public invokeAuthorizeWeb({ callbackId = '' } = {}): string {
+    return this.generateURI({
+      routeName: 'eos/authorizeInWeb',
+      params: {},
+      callbackId
+    })
+  }
 
-  // getAccount(accountName: string, callback: any) {
-  //   this.eos.getAccount({ account_name: accountName }).then((res: any) => {
-  //     callback && callback(null, res)
-  //   })
-  // }
+  // 发起转账请求
+  public invokeTransfer({
+    to = '',
+    amount = 0,
+    tokenName = 'EOS',
+    tokenContract = 'eosio.token',
+    tokenPrecision = 4,
+    memo = '',
+    orderInfo = '',
+    callbackId = ''
+  }): string {
+    return this.generateURI({
+      routeName: 'eos/transfer',
+      params: {
+        to,
+        amount,
+        tokenName,
+        tokenContract,
+        tokenPrecision,
+        memo,
+        orderInfo
+      },
+      callbackId
+    })
+  }
+
+  // 发起事务请求
+  public invokeTransaction({
+    actions = [],
+    options = { broadcast: true },
+    callbackId = ''
+  }): string {
+    return this.generateURI({
+      routeName: 'eos/transaction',
+      params: {
+        actions,
+        options
+      },
+      callbackId
+    })
+  }
+
+  // 获取帐号信息
+  public invokeAccountInfo({ callbackId = '' }): string {
+    return this.generateURI({
+      routeName: 'eos/account_info',
+      params: {},
+      callbackId
+    })
+  }
 }
--- test/meet-bridge.test.ts
+++ test/meet-bridge.test.ts
@@ -3,7 +3,7 @@
  * @Author: JohnTrump
  * @Date: 2018-08-06 16:25:08
  * @Last Modified by: JohnTrump
- * @Last Modified time: 2018-08-06 16:32:26
+ * @Last Modified time: 2018-08-07 15:07:36
  */
 
 import Bridge from '../src/meet-bridge'
@@ -24,7 +24,6 @@ describe('init testingv', () => {
 })
 
 describe('测试编码与对象之间的转换', () => {
-  const bridge = new Bridge()
   const targetObj = {
     to: 'wujunchuan12', // 收款地址
     amount: 1, // 金额
@@ -34,12 +33,105 @@ describe('测试编码与对象之间的转换', () => {
     memo: '我只是来测试的', // 这个Memo可能是合约需要用到的参数，与订单详情说明的意义不一样，订单详情说明只是给用户展示的一个字段，而memo是合约要用到的
     orderInfo: '邀请码购买' // 订单详情说明
   }
-  const result = bridge._coverObjectToParams(targetObj)
+  const result = Bridge.coverObjectToParams(targetObj)
 
-  const reverObj = bridge._revertParamsToObject(result)
+  const reverObj = Bridge.revertParamsToObject(result)
 
   it('编码与对象之间的转换', () => {
     // 编码与解编码后的对象应该是一致的
     expect(reverObj).toEqual(targetObj)
+  })
+})
+
+describe('生成协议URI地址', () => {
+  const bridge = new Bridge()
+  const url = bridge.generateURI({
+    routeName: 'eos/account_info',
+    params: {}
+  })
+  it('生成协议URI地址', () => {
+    expect(url).toEqual(expect.stringMatching('eos/account_info'))
+  })
+})
+
+describe('测试ParseURL', () => {
+  const url = 'meetone://eos/authorizeInWeb?params=JTdCJTdE&callbackId=100#MyHash'
+  it('返回source', () => {
+    expect(Bridge.parseURL(url)).toMatchObject({
+      source: url
+    })
+  })
+  it('解析出URL的关键信息', () => {
+    expect(Bridge.parseURL(url)).toMatchObject({
+      source: url,
+      protocol: 'meetone'
+    })
+  })
+  it('获取参数', () => {
+    const urlObj = Bridge.parseURL(url)
+    const paramsObj = urlObj.params
+    expect(Bridge.revertParamsToObject(paramsObj.params)).toEqual({})
+  })
+})
+
+describe('测试SDK', () => {
+  const bridge = new Bridge()
+  it('请求获取授权', () => {
+    const url = bridge.invokeAuthorizeWeb({ callbackId: '100' })
+    const urlObj = Bridge.parseURL(url)
+    const paramsObj = urlObj.params
+    expect(url).toEqual(expect.stringMatching('eos/authorizeInWeb'))
+    expect(Bridge.revertParamsToObject(paramsObj.params)).toEqual({})
+  })
+
+  it('发起转账请求', () => {
+    const transferConfig = {
+      to: 'wujunchuan12', // 收款地址
+      amount: 1, // 金额
+      tokenName: 'EOS', // 代币名称， 转账金额传参需要带上
+      tokenContract: 'eosio.token', // Token合约地址
+      tokenPrecision: 4, // token的精度
+      memo: '我只是来测试的', // 这个Memo可能是合约需要用到的参数，与订单详情说明的意义不一样，订单详情说明只是给用户展示的一个字段，而memo是合约要用到的
+      orderInfo: '邀请码购买' // 订单详情说明
+    }
+    const url = bridge.invokeTransfer(Object.assign({}, transferConfig, { callbackId: '100' }))
+    const urlObj = Bridge.parseURL(url)
+    const paramsObj = urlObj.params
+    expect(url).toEqual(expect.stringMatching('eos/transfer'))
+    expect(Bridge.revertParamsToObject(paramsObj.params)).toEqual(transferConfig)
+  })
+
+  it('发起事务请求', () => {
+    const transactionActions = [
+      {
+        account: 'eosio.token',
+        name: 'transfer',
+        authorization: [
+          {
+            actor: 'wujunchuan11', // creator
+            permission: 'active'
+          }
+        ],
+        data: {
+          from: 'wujunchuan11', // creator
+          to: 'wujunchuan12',
+          quantity: '1.0000 EOS',
+          memo: 'transaction-bridge test'
+        }
+      }
+    ]
+
+    const url = bridge.invokeTransaction({
+      actions: transactionActions
+    })
+    const urlObj = Bridge.parseURL(url)
+    const paramsObj = urlObj.params
+    expect(url).toEqual(expect.stringMatching('eos/transaction'))
+    expect(Bridge.revertParamsToObject(paramsObj.params).actions).toEqual(transactionActions)
+  })
+
+  it('获取帐号信息', () => {
+    const url = bridge.invokeAccountInfo({})
+    expect(url).toEqual(expect.stringMatching('eos/account_info'))
   })
 })
